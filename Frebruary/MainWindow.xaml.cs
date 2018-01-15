a5009b042887af920edf0c433ea0cb70f2b03757
Abhilash Rejanair <1995ad@gmail.com>
2018-01-15 21:00:12 +0530

    - Fixed error code bug - Fixed browse page bug (using Windows.Forms file picker) - Fixed previewer bug

--- Frebruary/MainWindow.xaml.cs
+++ Frebruary/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.WindowsAPICodePack.Dialogs;
 using System.IO;
 using System.Xml;
 using System.Text.RegularExpressions;
@@ -36,7 +36,9 @@ namespace Frebruary
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var dlg = new CommonOpenFileDialog();
+            /* Removed use of Windows Core API Code Pack
+             * 
+             * var dlg = new CommonOpenFileDialog();
             dlg.Title = "Browse to the Freb folder";
             dlg.IsFolderPicker = true;
 
@@ -55,11 +57,28 @@ namespace Frebruary
                 locationTextBox.Text = dlg.FileName;
 
             }
+            */
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                FolderBrowserDialog fbroswer = new System.Windows.Forms.FolderBrowserDialog(); ;
+                System.Windows.Forms.DialogResult result = fbroswer.ShowDialog();
 
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    locationTextBox.Text = fbroswer.SelectedPath;
+                }
+
+            }
             // Process open file dialog box results
 
         }
-
+        private void OnKeyDownHandler(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                scanButton_Click(sender, e);
+            }
+        }
         public void scanButton_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -72,9 +91,17 @@ namespace Frebruary
                 List<Freb> source = processedList(myFiles);
                 DataGrid.ItemsSource = source;
             }
-            catch(Exception)
+            catch(System.UnauthorizedAccessException)
             {
-                MessageBox.Show("Could not scan the directory. Is it valid? Do you have sufficient privileges?");
+                System.Windows.MessageBox.Show("Could not scan the directory. Insufficient privileges to open the folder.");
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                System.Windows.MessageBox.Show("Could not scan the directory. Directory not found.");
+            }
+            catch (Exception e1)
+            {
+                System.Windows.MessageBox.Show("Could not scan the directory. \nException: "+e1.ToString());
             }
         }
 
@@ -88,15 +115,28 @@ namespace Frebruary
                 XmlNode node = doc.DocumentElement.SelectSingleNode("/failedRequest");
 
 
-                XmlNodeList error_node = doc.GetElementsByTagName("freb:Description");
+                XmlNodeList error_node = doc.GetElementsByTagName("Data");
+                Console.Write(error_node.Count);
                 string val = "", error_info="NA";
                 foreach(XmlNode info in error_node)
                 {
                     val = info.InnerText;
-                    Match match = Regex.Match(val, @"\((0x.*)\d{3,8}\)");
-                    if(match.Success)
+                    if (val == "0") continue;
+                    //Match match = Regex.Match(val, @"\((0x.*)\d{3,8}\)");
+                    // New error code regex : .*ErrorCode.*(\d[10]).*
+                    //Match match = Regex.Match(val, @"ErrorCode.*(\d{10})");
+                    /*
+                    if (match.Success)
                     {
                         error_info = match.Value;
+                    }*/
+                    if(info.Attributes!= null)
+                    {
+                        if(info.Attributes["Name"]!=null)
+                        {
+                            if(info.Attributes["Name"].Value=="ErrorCode")
+                                error_info = val;
+                        }
                     }
                 }
                 source.Add(new Freb()
@@ -125,19 +165,27 @@ namespace Frebruary
             var row = (Freb)DataGrid.SelectedItem;
             if(row == null)
             {
-                MessageBox.Show("Click on a Freb to load the previewer");
+                System.Windows.MessageBox.Show("Click on a Freb to load the previewer");
                 return;
             }
             string path = row.path;
-            if(pre==null)
+            if(pre == null)
             {
                 pre = new Previewer(path);
                 pre.Show();
             }
             else
             {
-                // If already open,bring to focus
-                pre.Activate();
+                if (pre.IsLoaded == false)
+                {
+                    pre = new Previewer(path);
+                    pre.Show();
+                }
+                else
+                {
+                    // If already open,bring to focus
+                    pre.Activate();
+                }
             }
         }
 
@@ -163,7 +211,7 @@ namespace Frebruary
         {
             if(source == null)
             {
-                MessageBox.Show("Scan and populate the Freb entries before applying a filter");
+                System.Windows.MessageBox.Show("Scan and populate the Freb entries before applying a filter");
                 return;
             }
             Filter fil = new Filter(this);
@@ -192,16 +240,3 @@ namespace Frebruary
         }
     }
 }
-
-
-/*
- url
-siteId
-appPoolId
-processId
-verb
-authenticationType
-failureReason
-statusCode
-timeTaken
-*/
