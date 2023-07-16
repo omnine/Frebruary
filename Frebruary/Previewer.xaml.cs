8c8dc92bcdcc94685da46ae93a1bed4d4b4faa78
Mingfa Ma <mingfa.ma@deepnetsecurity.com>
2023-07-16 17:05:54 +0100

    add webview

--- Frebruary/Previewer.xaml.cs
+++ Frebruary/Previewer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Web.WebView2.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,16 @@ namespace Frebruary
     {
         public string path;
 
+
+        private async Task InitializeAsync(Uri s)
+        {
+            CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--allow-file-access-from-files");
+            CoreWebView2Environment environment = await CoreWebView2Environment.CreateAsync(null, null, options);
+            await browser2.EnsureCoreWebView2Async(environment);
+//            browser2.CoreWebView2.Navigate("https://yourlink.jnlp");
+            browser2.Source = s;
+        }
+
         public Previewer(string spath)
         {
             InitializeComponent();
@@ -33,6 +44,8 @@ namespace Frebruary
                 s = new Uri(path);
                 browser.Source = s;
                 this.Title = "Previewer - "+path;
+
+                _ = InitializeAsync(s);
             }
         }
 
