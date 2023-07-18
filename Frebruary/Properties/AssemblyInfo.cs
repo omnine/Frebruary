223b8315a5e83eb343792fae2fce2481db9dc82f
Mingfa Ma <mingfa.ma@deepnetsecurity.com>
2023-07-18 15:34:06 +0100

    version 12.0.01

--- Frebruary/Properties/AssemblyInfo.cs
+++ Frebruary/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Frebruary")]
+[assembly: AssemblyTitle("FrebViewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Frebruary")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("FrebViewer")]
+[assembly: AssemblyCopyright("Copyright ©  Omnine 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("12.0.0.1")]
+[assembly: AssemblyFileVersion("12.0.0.1")]
--- Frebruary/Properties/Resources.Designer.cs
+++ Frebruary/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Frebruary.Properties {
+namespace FrebViewer.Properties {
     using System;
     
     
@@ -19,7 +19,7 @@ namespace Frebruary.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -39,7 +39,7 @@ namespace Frebruary.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Frebruary.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FrebViewer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -61,7 +61,7 @@ namespace Frebruary.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Abhilash Rejanair.
+        ///   Looks up a localized string similar to Omnine.
         /// </summary>
         internal static string Publisher {
             get {
@@ -70,7 +70,7 @@ namespace Frebruary.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 2.0.
+        ///   Looks up a localized string similar to 12.0.
         /// </summary>
         internal static string version {
             get {
